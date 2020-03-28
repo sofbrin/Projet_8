a7e6b7170fcfbab035a1c886307f4e9dac7d6fe4
sofbrin <sofbrin@hotmail.com>
2020-03-28 23:14:18 +0100

    correction settings

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 if os.environ.get('ENV') == 'PRODUCTION':
 
@@ -146,7 +146,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 
     # Extra places for collectstatic to find static files
     STATICFILES_DIRS = (
-        normpath(join(PROJECT_ROOT, 'static')),
+        os.path.join(PROJECT_ROOT, 'static'),
     )
 
     # Simplified static file serving
