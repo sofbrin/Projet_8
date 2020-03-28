e9df6cc40c2e9a9c85d2f5213746d03f774f576f
sofbrin <sofbrin@hotmail.com>
2020-03-28 18:48:54 +0100

    correction erreur settings

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -144,7 +144,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 
     # Extra places for collectstatic to find static files
     STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static')
+        os.path.join(PROJECT_ROOT, 'static'),
     )
 
     # Simplified static file serving
