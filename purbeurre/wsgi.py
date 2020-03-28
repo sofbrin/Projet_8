448f707c6069bd115c4bd7589655e20c154197b9
sofbrin <sofbrin@hotmail.com>
2020-03-28 21:07:27 +0100

    correction wsgi

--- purbeurre/wsgi.py
+++ purbeurre/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for purbeurre1 project.
+WSGI config for purbeurre project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'purbeurre1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'purbeurre.settings')
 
 application = get_wsgi_application()
