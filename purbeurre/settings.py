c9d44971a89a07ade412316a551a9cd0d4784f6e
sofbrin <sofbrin@hotmail.com>
2020-04-01 16:28:04 +0200

    settings update

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -141,7 +141,7 @@ STATIC_URL = '/static/'
 if os.environ.get('ENV') == 'PRODUCTION':
 
     # Static files settings
-    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+    PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
