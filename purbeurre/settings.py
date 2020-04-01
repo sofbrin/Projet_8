287699dcda65b83549693ce839d126a216d10835
sofbrin <sofbrin@hotmail.com>
2020-04-01 15:44:06 +0200

    settings update

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -152,7 +152,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 
     # Simplified static file serving
     # https://warehouse.python.org/project/whitenoise
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
