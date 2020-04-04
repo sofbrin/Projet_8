09983357d2d6e21319f34cc839fb38887ee97cd8
sofbrin <sofbrin@hotmail.com>
2020-04-04 21:14:47 +0200

    update

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -141,9 +141,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 if os.environ.get('ENV') == 'PRODUCTION':
 
     # Extra places for collectstatic to find static files
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
+    #STATICFILES_DIRS = (
+        #os.path.join(BASE_DIR, 'static'),
+    #)
 
     # Simplified static file serving
     # https://warehouse.python.org/project/whitenoise
