8dd20c30ce42e1486973a33f85e0e2fa5b986a5c
sofbrin <sofbrin@hotmail.com>
2020-04-04 20:44:20 +0200

    update

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -152,4 +152,4 @@ if os.environ.get('ENV') == 'PRODUCTION':
     #db_from_env = dj_database_url.config(conn_max_age=500)
     #DATABASES['default'].update(db_from_env)
 
-django_heroku.settings(locals())
+    django_heroku.settings(locals())
